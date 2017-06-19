34c8b4af260f0825d3d71efffe2ca46eb514ab85
Gina Trapani <ginatrapani@gmail.com>
2017-06-18 23:05:00 -0400

    fix: use dolla signs at the zsh/bash prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -54,7 +54,7 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(battery-status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(battery-status)in $(directory_name) $(git_dirty)$(need_push)\n$ '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
