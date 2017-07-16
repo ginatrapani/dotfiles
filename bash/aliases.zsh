ad139124722d3c66312807a23817c98700e8d322
Gina Trapani <ginatrapani@gmail.com>
2017-07-16 15:39:10 -0400

    feat: add goals alias for hashtag-goals

--- bash/aliases.zsh
+++ bash/aliases.zsh
@@ -3,3 +3,6 @@ alias t='todo.sh -a -t '
 
 # easy way to switch to bash for todo.txt autocompletion
 alias b='exec bash -l'
+
+# shortcut to #goals.py
+alias goals='cd ~/Documents/mydata/code/hashtag-goals/;python3 \#goals.py --todo ~/Dropbox/notesy/todo/todo.txt --goals ~/Dropbox/notesy/todo/#goals.txt  --done ~/Dropbox/notesy/todo/done.txt'
