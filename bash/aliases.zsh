20aef99cb86404e0e044fd7902b775f15f9b4f14
Gina Trapani <ginatrapani@gmail.com>
2017-11-22 14:04:42 -0800

    feat: add goals2dayone alias

--- bash/aliases.zsh
+++ bash/aliases.zsh
@@ -6,3 +6,6 @@ alias b='exec bash -l'
 
 # shortcut to #goals.py
 alias goals='cd ~/Documents/mydata/code/hashtag-goals/;python3 \#goals.py --todo ~/Dropbox/notes/todo/todo.txt --goals ~/Dropbox/notes/todo/#goals.txt  --done ~/Dropbox/notes/todo/done.txt'
+
+# shortcut to pipe weekly review to Day One
+alias goals2dayone='cat <(echo "# Week `date +%V`: Weekly Review + Goal progress report")  ~/Dropbox/notes/todo/goal-report-header.txt <(goals) | dayone2 new'
